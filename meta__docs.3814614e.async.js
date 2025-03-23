47991dc51e46c652b1f7cdb0c7df2b83da6389e4
evanfang0054 <380616679@qq.com>
2025-03-23 04:09:26 +0000

    Deploying to build from @ evanfang0054/efdev@778fdaca87eed28a9d2047f4a3cf03807c778e1b 🚀

---
+++ meta__docs.3814614e.async.js
@@ -0,0 +1,203 @@
+"use strict";(self.webpackChunkefdev=self.webpackChunkefdev||[]).push([[904],{10002:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(95127),o={}},15665:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(64354),o={}},30084:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(63845),o={}},29491:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(6658),o={}},78262:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(90357),o={}},98159:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(19670),o={}},86375:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(17395),o={}},70954:function(t,e,n){n.r(e),n.d(e,{texts:function(){return a}});var d=n(95127);const a=[{value:"\u652F\u6301\u914D\u5957\u7684 ",paraId:0,tocIndex:0},{value:"commitlint \u914D\u7F6E",paraId:0,tocIndex:0},{value:"\uFF0C\u7528\u4E8E\u5BF9 ",paraId:0,tocIndex:0},{value:"git commit message",paraId:0,tocIndex:0},{value:" \u8FDB\u884C\u6821\u9A8C\u3002",paraId:0,tocIndex:0},{value:"\u4F7F\u7528\u65F6\uFF0C\u9700\u8981\u5B89\u88C5 ",paraId:1,tocIndex:1},{value:"@commitlint/cli",paraId:1,tocIndex:1},{value:"\uFF1A",paraId:1,tocIndex:1},{value:`npm install @efdev/config-commitlint -D
+`,paraId:2,tocIndex:1},{value:`npm install @commitlint/cli -D
+`,paraId:3,tocIndex:1},{value:"\u5728 ",paraId:4,tocIndex:2},{value:"commitlint.config.js",paraId:4,tocIndex:2},{value:" \u4E2D\u96C6\u6210\u672C\u5305:",paraId:4,tocIndex:2},{value:`module.exports = {
+  extends: ['@efdev/config-commitlint'],
+};
+`,paraId:5,tocIndex:2},{value:"\u53EF\u901A\u8FC7 ",paraId:6,tocIndex:3},{value:"husky",paraId:6,tocIndex:3},{value:" \u8BBE\u7F6E\u5728 ",paraId:6,tocIndex:3},{value:"git commit",paraId:6,tocIndex:3},{value:" \u65F6\u89E6\u53D1 ",paraId:6,tocIndex:3},{value:"commitlint",paraId:6,tocIndex:3},{value:"\u3002",paraId:6,tocIndex:3},{value:"\u9996\u5148\u5B89\u88C5 husky\uFF1A",paraId:7,tocIndex:3},{value:`npm install husky --save-dev
+`,paraId:8,tocIndex:3},{value:"\u7136\u540E\u6267\u884C\u6DFB\u52A0",paraId:9,tocIndex:3},{value:"commit-msg",paraId:9,tocIndex:3},{value:":",paraId:9,tocIndex:3},{value:`npx husky add .husky/commit-msg 'npx commitlint --edit "\${1}"'
+`,paraId:10,tocIndex:3},{value:"\u66F4\u591A\u4FE1\u606F\u53EF\u53C2\u8003 ",paraId:11,tocIndex:3},{value:"commitlint \u6587\u6863",paraId:11,tocIndex:3},{value:"\u3002",paraId:11,tocIndex:3}]},4381:function(t,e,n){n.r(e),n.d(e,{texts:function(){return a}});var d=n(64354);const a=[{value:"\u652F\u6301\u914D\u5957\u7684 ",paraId:0,tocIndex:0},{value:"markdownlint \u53EF\u5171\u4EAB\u914D\u7F6E",paraId:0,tocIndex:0},{value:"\u3002",paraId:0,tocIndex:0},{value:"\u9700\u8981\u5148\u5168\u5C40\u5B89\u88C5 ",paraId:1,tocIndex:1},{value:"markdownlint-cli",paraId:1,tocIndex:1},{value:`npm install -g markdownlint-cli
+`,paraId:2,tocIndex:1},{value:"\u518D\u5B89\u88C5 ",paraId:3,tocIndex:1},{value:"markdownlint",paraId:3,tocIndex:1},{value:"\uFF1A",paraId:3,tocIndex:1},{value:`npm install @efdev/config-markdownlint -D
+`,paraId:4,tocIndex:1},{value:`npm install markdownlint -D
+`,paraId:5,tocIndex:1},{value:"\u5728 ",paraId:6,tocIndex:2},{value:".markdownlint.json",paraId:6,tocIndex:2},{value:" \u4E2D\u7EE7\u627F\u672C\u5305:",paraId:6,tocIndex:2},{value:`{
+  "extends": "@efdev/config-markdownlint"
+}
+`,paraId:7,tocIndex:2}]},49673:function(t,e,n){n.r(e),n.d(e,{texts:function(){return a}});var d=n(63845);const a=[{value:"\u652F\u6301\u914D\u5957\u7684 ",paraId:0,tocIndex:0},{value:"stylelint \u53EF\u5171\u4EAB\u914D\u7F6E",paraId:0,tocIndex:0},{value:"\u3002",paraId:0,tocIndex:0},{value:"\u9700\u8981\u5148\u884C\u5B89\u88C5 ",paraId:1,tocIndex:1},{value:"stylelint",paraId:1,tocIndex:1},{value:" \u3001 ",paraId:1,tocIndex:1},{value:"stylelint-scss",paraId:1,tocIndex:1},{value:"\u3001 ",paraId:1,tocIndex:1},{value:"stylelint-order",paraId:1,tocIndex:1},{value:"\uFF1A",paraId:1,tocIndex:1},{value:`npm install @efdev/config-stylelint -D
+`,paraId:2,tocIndex:1},{value:`npm install stylelint stylelint-scss stylelint-order -D
+`,paraId:3,tocIndex:1},{value:"\u5728 ",paraId:4,tocIndex:2},{value:".stylelintrc",paraId:4,tocIndex:2},{value:" \u4E2D\u7EE7\u627F\u672C\u5305:",paraId:4,tocIndex:2},{value:`{
+  "extends": "@efdev/config-stylelint"
+}
+`,paraId:5,tocIndex:2}]},34588:function(t,e,n){n.r(e),n.d(e,{texts:function(){return a}});var d=n(6658);const a=[{value:"Git \u6BCF\u6B21\u63D0\u4EA4\u4EE3\u7801\uFF0C\u90FD\u8981\u5199 Commit message\uFF0C\u5426\u5219\u4E0D\u5141\u8BB8\u63D0\u4EA4",paraId:0,tocIndex:0},{value:"\u6BCF\u6B21\u63D0\u4EA4\uFF0CCommit message \u90FD\u5305\u62EC\u4E09\u4E2A\u90E8\u5206\uFF1AHeader\uFF0CBody(\u975E\u5FC5\u586B) \u548C Footer(\u975E\u5FC5\u586B)",paraId:1,tocIndex:1},{value:"\u63D0\u4EA4\u4FE1\u606F\u6B63\u6587\u5F00\u59CB\u524D\u5FC5\u987B\u6709\u4E00\u4E2A\u7A7A\u884C",paraId:1,tocIndex:1},{value:"\u63D0\u4EA4\u4FE1\u606F\u811A\u6CE8\u5F00\u59CB\u524D\u5FC5\u987B\u6709\u4E00\u4E2A\u7A7A\u884C",paraId:1,tocIndex:1},{value:"\u4E0D\u5141\u8BB8\u7A7A\u7684\u4E3B\u9898",paraId:1,tocIndex:1},{value:"\u4E3B\u9898\u540E\u4E0D\u5141\u8BB8\u6216\u5FC5\u987B\u6709\u53E5\u53F7",paraId:1,tocIndex:1},{value:"\u4E0D\u5141\u8BB8\u7A7A\u7684\u7C7B\u578B",paraId:1,tocIndex:1},{value:`<type>(<scope>): <subject>
+# \u7A7A\u4E00\u884C
+<body>
+# \u7A7A\u4E00\u884C
+<footer>
+`,paraId:2,tocIndex:1},{value:"Header \u90E8\u5206\u53EA\u6709\u4E00\u884C\uFF0C\u5305\u62EC\u4E09\u4E2A\u5B57\u6BB5\uFF1Atype\uFF08\u5FC5\u9700\uFF09\u3001scope\uFF08\u53EF\u9009\uFF09\u548C subject\uFF08\u5FC5\u9700\uFF09",paraId:3,tocIndex:2},{value:"type \u7528\u4E8E\u8BF4\u660E commit \u7684\u7C7B\u522B\uFF0C\u53EA\u5141\u8BB8\u4F7F\u7528\u4E0B\u9762 13 \u4E2A\u6807\u8BC6",paraId:4,tocIndex:3},{value:`feat\uFF1A\u65B0\u589E\u529F\u80FD
+fix\uFF1Abug\u4FEE\u590D
+build\uFF1A\u4E3B\u8981\u6BCD\u7684\u662F\u4FEE\u6539\u9879\u76EE\u6784\u5EFA\u7CFB\u7EDF\uFF08\u4F8B\u5982glup\uFF0Cwebpack\uFF0Crollup\uFF0Ctsconfig\u7684\u914D\u7F6E\u7B49\uFF09\u7684\u63D0\u4EA4
+merge\uFF1A\u5206\u652F\u5408\u5E76
+upd\uFF1A\u66F4\u65B0\u67D0\u529F\u80FD\uFF08\u4E0D\u662Ffeat\uFF0C\u4E0D\u662Ffix\uFF09
+test\uFF1A\u65B0\u589E\u6D4B\u8BD5\u7528\u4F8B\u6216\u66F4\u65B0\u73B0\u6709\u6D4B\u8BD5
+ci\uFF1A\u4E3B\u8981\u76EE\u7684\u662F\u4FEE\u6539\u9879\u76EE\u6301\u7EED\u96C6\u6210\u6D41\u7A0B\uFF08\u4F8B\u5982Travis\uFF0CJenkins\uFF0CGitlab CI\uFF0CCircle\u7B49\uFF09\u7684\u63D0\u4EA4
+docs\uFF1A\u6587\u6863\u66F4\u65B0
+perf\uFF1A\u6027\u80FD\uFF0C\u4F53\u9A8C\u4F18\u5316
+refactor\uFF1A\u91CD\u6784\u4EE3\u7801
+style\uFF1A\u4E0D\u5F71\u54CD\u7A0B\u5EA6\u903B\u8F91\u7684\u4EE3\u7801\u4FEE\u6539\uFF08\u4FEE\u6539\u7A7A\u767D\u5B57\u7B26\uFF0C\u683C\u5F0F\u7F29\u8FDB\uFF0C\u8865\u5168\u7F3A\u5931\u7684\u5206\u53F7\u7B49\uFF0C\u6CA1\u6709\u6539\u53D8\u4EE3\u7801\u903B\u8F91\uFF09
+revert\uFF1A\u56DE\u6EDA\u67D0\u4E2A\u66F4\u65E9\u4E4B\u524D\u7684\u7248\u672C
+chore\uFF1A\u6784\u5EFA\u8FC7\u7A0B\u6216\u8F85\u52A9\u5DE5\u5177\u7684\u53D8\u52A8
+`,paraId:5,tocIndex:3},{value:"scope \u7528\u4E8E\u8BF4\u660E commit \u5F71\u54CD\u7684\u8303\u56F4\uFF0C\u6BD4\u5982\u6570\u636E\u5C42\u3001\u63A7\u5236\u5C42\u3001\u89C6\u56FE\u5C42\u7B49\u7B49\uFF0C\u89C6\u9879\u76EE\u4E0D\u540C\u800C\u4E0D\u540C",paraId:6,tocIndex:4},{value:"subject \u662F commit \u76EE\u7684\u7684\u7B80\u77ED\u63CF\u8FF0\uFF0C\u4E0D\u8D85\u8FC7 100 \u4E2A\u5B57\u7B26",paraId:7,tocIndex:5}]},93823:function(t,e,n){n.r(e),n.d(e,{texts:function(){return a}});var d=n(90357);const a=[{value:"\u9879\u76EE\u6587\u6863\u5EFA\u8BAE\u4F7F\u7528",paraId:0,tocIndex:0},{value:"dumi",paraId:0,tocIndex:0},{value:"\u8FDB\u884C\u7F16\u5199",paraId:0,tocIndex:0},{value:"\u591A\u68C0\u67E5\uFF0C\u786E\u4FDD\u6CA1\u6709\u9519\u522B\u5B57",paraId:1,tocIndex:1},{value:"\u8A00\u7B80\u610F\u8D45",paraId:1,tocIndex:1},{value:"\u6CE8\u610F\u7279\u6B8A\u540D\u8BCD\u7684\u5927\u5C0F\u5199\uFF1AAJAX\u3001Highcharts\u3001React\u3001JavaScript\u3001HTML\uFF0C\u65E0\u8BBA\u662F\u5426\u5728\u53E5\u9996\u90FD\u5E94\u8BE5\u4EE5\u540C\u6837\u7684\u65B9\u5F0F\u5199\uFF08\u6709 lint \u5DE5\u5177\u9650\u5236\uFF09",paraId:1,tocIndex:1},{value:"\u53EA\u6709\u4E2D\u6587\u6216\u4E2D\u82F1\u6587\u6DF7\u6392\u4E2D\uFF0C\u4E00\u5F8B\u4F7F\u7528\u4E2D\u6587/\u5168\u89D2\u6807\u70B9",paraId:1,tocIndex:1},{value:"\u4E25\u683C\u9075\u5FAA",paraId:2,tocIndex:2},{value:"markdownlint",paraId:2,tocIndex:2},{value:"\u683C\u5F0F",paraId:2,tocIndex:2},{value:`[
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Vue.js",
+  "Angular",
+  "jQuery",
+  "Bootstrap",
+  "Sass",
+  "Less",
+  "Webpack",
+  "Babel",
+  "NPM",
+  "Yarn",
+  "Git",
+  "GitHub",
+  "GitLab",
+  "Bitbucket",
+  "ESLint",
+  "Prettier",
+  "Node.js",
+  "Express",
+  "Koa",
+  "Next.js",
+  "Nuxt.js",
+  "Gatsby",
+  "TypeScript",
+  "Ember.js",
+  "Backbone.js",
+  "Marionette.js",
+  "React Native",
+  "Ionic",
+  "Cordova",
+  "Electron",
+  "Tailwind CSS",
+  "Foundation",
+  "Materialize",
+  "Semantic UI",
+  "Ant Design",
+  "Figma",
+  "Sketch",
+  "Adobe XD",
+  "InVision",
+  "Zeplin",
+  "Storybook",
+  "Jest",
+  "Mocha",
+  "Chai",
+  "Sinon",
+  "Enzyme",
+  "React Testing Library",
+  "Cypress",
+  "Selenium",
+  "Puppeteer",
+  "WebdriverIO",
+  "Redux",
+  "MobX",
+  "Vuex",
+  "NgRx",
+  "Apollo Client",
+  "Relay",
+  "GraphQL",
+  "RESTful API",
+  "AJAX",
+  "Fetch API",
+  "XMLHttpRequest",
+  "Socket.IO",
+  "D3.js",
+  "Three.js",
+  "Chart.js",
+  "Highcharts",
+  "jQuery UI",
+  "Lodash",
+  "Underscore.js",
+  "Moment.js",
+  "Date-fns",
+  "Numeral.js",
+  "Axios",
+  "jQuery.ajax",
+  "Vue Router",
+  "React Router",
+  "Angular Router",
+  "History API",
+  "Service Workers",
+  "Web Workers",
+  "WebAssembly",
+  "CSS Grid",
+  "CSS Flexbox",
+  "BEM",
+  "OOCSS",
+  "SMACSS",
+  "Atomic Design",
+  "PostCSS",
+  "Autoprefixer",
+  "CSS Modules",
+  "Styled Components",
+  "Emotion",
+  "JSS",
+  "CSS-in-JS",
+  "Shadow DOM",
+  "Custom Elements",
+  "HTML Imports",
+  "Template Literals",
+  "Tagged Templates",
+  "Async/Await",
+  "Promises",
+  "Generators",
+  "Proxies",
+  "Maps",
+  "Sets",
+  "WeakMaps",
+  "WeakSets",
+  "Symbols",
+  "BigInt",
+  "Intl",
+  "URL",
+  "URLSearchParams",
+  "Blob",
+  "FileReader",
+  "Canvas",
+  "WebGL",
+  "Web Animations API",
+  "Intersection Observer API",
+  "Resize Observer",
+  "Mutation Observer",
+  "Pointer Events",
+  "Touch Events",
+  "Media Queries",
+  "CSS Variables",
+  "Web Storage API",
+  "IndexedDB",
+  "Cache API",
+  "Web SQL Database",
+  "File System Access API",
+  "WebRTC",
+  "Speech API",
+  "WebXR",
+  "Web Payments",
+  "Web Authentication",
+  "Web Notifications",
+  "Progressive Web Apps",
+  "Workbox",
+  "Lighthouse",
+  "Google Tag Manager",
+  "Adobe Dreamweaver",
+  "Brackets",
+  "Visual Studio Code",
+  "Atom",
+  "Sublime Text",
+  "WebStorm",
+  "Eslint-plugin-react",
+  "Vue Server Renderer",
+  "Angular Universal",
+  "Preact",
+  "Svelte",
+  "11ty",
+  "Hugo",
+  "Gatsby Image",
+  "Netlify CMS",
+  "Contentful",
+  "Dockerize",
+  "Jamstack",
+  "AMP",
+  "Polyfill"
+]
+`,paraId:3,tocIndex:3}]},92923:function(t,e,n){n.r(e),n.d(e,{texts:function(){return a}});var d=n(19670);const a=[{value:"\u4E2A\u4EBA\u98CE\u683C\u9879\u76EE\u5DE5\u5177/\u914D\u7F6E",paraId:0,tocIndex:0}]},66276:function(t,e,n){n.r(e),n.d(e,{texts:function(){return a}});var d=n(17395);const a=[]}}]);
