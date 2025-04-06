537aef77e5d0873a322d05bc7fa4249d9494f0a7
evanfang0054 <380616679@qq.com>
2025-04-06 09:42:53 +0000

    Deploying to build from @ evanfang0054/efdev@f3feaab9ca0a232043f1b9121e28af4184cb2f90 🚀

--- meta__docs.98c9d089.async.js
+++ meta__docs.98c9d089.async.js
@@ -1,4 +1,74 @@
-"use strict";(self.webpackChunkefdev=self.webpackChunkefdev||[]).push([[904],{57086:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(7120),o={}},95278:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(21672),o={}},46075:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(68467),o={}},16466:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(16180),o={}},82305:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(79475),o={}},29829:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(91963),o={}},70488:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(4154),o={}},30947:function(t,e,n){n.r(e),n.d(e,{texts:function(){return a}});var d=n(7120);const a=[{value:"\u652F\u6301\u914D\u5957\u7684 ",paraId:0,tocIndex:0},{value:"commitlint \u914D\u7F6E",paraId:0,tocIndex:0},{value:"\uFF0C\u7528\u4E8E\u5BF9 ",paraId:0,tocIndex:0},{value:"git commit message",paraId:0,tocIndex:0},{value:" \u8FDB\u884C\u6821\u9A8C\u3002",paraId:0,tocIndex:0},{value:"\u4F7F\u7528\u65F6\uFF0C\u9700\u8981\u5B89\u88C5 ",paraId:1,tocIndex:1},{value:"@commitlint/cli",paraId:1,tocIndex:1},{value:"\uFF1A",paraId:1,tocIndex:1},{value:`npm install @efdev/config-commitlint -D
+"use strict";(self.webpackChunkefdev=self.webpackChunkefdev||[]).push([[904],{95835:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(49449),o={}},57086:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(7120),o={}},95278:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(21672),o={}},46075:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(68467),o={}},16466:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(16180),o={}},82305:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(79475),o={}},29829:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(91963),o={}},70488:function(t,e,n){n.r(e),n.d(e,{demos:function(){return o}});var d=n(75271),a=n(4154),o={}},1126:function(t,e,n){n.r(e),n.d(e,{texts:function(){return a}});var d=n(49449);const a=[{value:"ESLint \u914D\u7F6E\u5305\uFF0C\u63D0\u4F9B\u7EDF\u4E00\u7684\u4EE3\u7801\u89C4\u8303\u548C\u683C\u5F0F\u5316\u89C4\u5219\u3002",paraId:0,tocIndex:0},{value:"\u{1F3AF} \u7EDF\u4E00\u7684\u4EE3\u7801\u98CE\u683C\u89C4\u8303",paraId:1,tocIndex:1},{value:"\u{1F50D} \u4E25\u683C\u7684\u4EE3\u7801\u8D28\u91CF\u68C0\u67E5",paraId:1,tocIndex:1},{value:"\u26A1\uFE0F \u652F\u6301 TypeScript",paraId:1,tocIndex:1},{value:"\u{1F6E0} \u53EF\u6269\u5C55\u7684\u89C4\u5219\u914D\u7F6E",paraId:1,tocIndex:1},{value:`config-eslint/
+\u251C\u2500\u2500 rules/                # ESLint \u89C4\u5219\u914D\u7F6E
+\u2502   \u251C\u2500\u2500 base/            # \u57FA\u7840\u89C4\u5219
+\u2502   \u2502   \u251C\u2500\u2500 best-practices.js    # \u6700\u4F73\u5B9E\u8DF5
+\u2502   \u2502   \u251C\u2500\u2500 possible-errors.js   # \u53EF\u80FD\u7684\u9519\u8BEF
+\u2502   \u2502   \u251C\u2500\u2500 style.js            # \u4EE3\u7801\u98CE\u683C
+\u2502   \u2502   \u251C\u2500\u2500 variables.js        # \u53D8\u91CF\u76F8\u5173
+\u2502   \u2502   \u251C\u2500\u2500 es6.js             # ES6+ \u7279\u6027
+\u2502   \u2502   \u2514\u2500\u2500 strict.js          # \u4E25\u683C\u6A21\u5F0F
+\u2502   \u2514\u2500\u2500 imports/        # \u5BFC\u5165/\u5BFC\u51FA\u89C4\u5219
+\u251C\u2500\u2500 typescript/         # TypeScript \u76F8\u5173\u914D\u7F6E
+\u251C\u2500\u2500 node.js            # Node.js \u73AF\u5883\u914D\u7F6E
+\u251C\u2500\u2500 index.js           # \u4E3B\u914D\u7F6E\u5165\u53E3
+\u251C\u2500\u2500 .eslintrc.js       # ESLint \u914D\u7F6E\u6587\u4EF6
+\u251C\u2500\u2500 .eslintignore      # ESLint \u5FFD\u7565\u914D\u7F6E
+`,paraId:2,tocIndex:2},{value:"\u5728\u9879\u76EE\u6839\u76EE\u5F55\u521B\u5EFA ",paraId:3,tocIndex:3},{value:".eslintrc.js",paraId:3,tocIndex:3},{value:" \u6587\u4EF6, \u5E76\u6DFB\u52A0\u4EE5\u4E0B\u914D\u7F6E:",paraId:3,tocIndex:3},{value:`pnpm add -D @efdev/config-eslint @babel/core @babel/eslint-parser eslint-plugin-import
+`,paraId:4,tocIndex:4},{value:`module.exports = {
+  extends: ['@efdev/config-eslint'],
+};
+`,paraId:5,tocIndex:4},{value:`pnpm add -D @efdev/config-eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint-plugin-import eslint-import-resolver-typescript
+`,paraId:6,tocIndex:5},{value:`module.exports = {
+  extends: ['@efdev/config-eslint/typescript'],
+};
+`,paraId:7,tocIndex:5},{value:`pnpm add -D @efdev/config-eslint @babel/core @babel/eslint-parser eslint-plugin-import eslint-config-egg
+`,paraId:8,tocIndex:6},{value:`module.exports = {
+  extends: ['@efdev/config-eslint/node'],
+};
+`,paraId:9,tocIndex:6},{value:`pnpm add -D @efdev/config-eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint-plugin-import eslint-import-resolver-typescript eslint-config-egg
+`,paraId:10,tocIndex:7},{value:`module.exports = {
+  extends: ['@efdev/config-eslint/node', '@efdev/config-eslint/typescript'],
+};
+`,paraId:11,tocIndex:7},{value:"\u4E3A\u4E86\u786E\u4FDD ESLint \u89C4\u5219\u4E0E Prettier \u683C\u5F0F\u5316\u89C4\u5219\u7684\u4E00\u81F4\u6027\uFF0C\u9700\u8981\u8FDB\u884C\u4EE5\u4E0B\u914D\u7F6E\uFF1A",paraId:12,tocIndex:8},{value:`# pnpm
+pnpm add -D eslint-config-prettier eslint-plugin-prettier
+
+# npm
+npm install --save-dev eslint-config-prettier eslint-plugin-prettier
+
+# yarn
+yarn add -D eslint-config-prettier eslint-plugin-prettier
+`,paraId:13,tocIndex:9},{value:"\u4FEE\u6539 ",paraId:14,tocIndex:10},{value:".eslintrc.js",paraId:14,tocIndex:10},{value:" \u914D\u7F6E\uFF0C\u6DFB\u52A0 Prettier \u652F\u6301\uFF1A",paraId:14,tocIndex:10},{value:`// \u57FA\u7840\u9879\u76EE
+module.exports = {
+  extends: ['@efdev/config-eslint', 'prettier'],
+};
+
+// TypeScript \u9879\u76EE
+module.exports = {
+  extends: ['@efdev/config-eslint/typescript', 'prettier'],
+};
+
+> **\u6CE8\u610F**\uFF1A\u786E\u4FDD \`prettier\` \u914D\u7F6E\u4F4D\u4E8E \`extends\` \u6570\u7EC4\u7684\u6700\u540E\u4F4D\u7F6E\uFF0C\u4EE5\u907F\u514D\u5176\u4ED6\u914D\u7F6E\u8986\u76D6 Prettier \u89C4\u5219\u3002
+
+## \u{1F4CB} \u53EF\u7528\u7684\u914D\u7F6E
+
+- \`@efdev/config-eslint\`: \u57FA\u7840\u914D\u7F6E
+- \`@efdev/config-eslint/typescript\`: TypeScript \u914D\u7F6E
+- \`@efdev/config-eslint/node\`: Node.js \u914D\u7F6E
+
+## \u{1F527} \u81EA\u5B9A\u4E49\u89C4\u5219
+
+\u53EF\u4EE5\u5728\u9879\u76EE\u7684 \`.eslintrc.js\` \u4E2D\u8986\u76D6\u9ED8\u8BA4\u89C4\u5219\uFF1A
+
+\`\`\`javascript
+module.exports = {
+  extends: ['@efdev/config-eslint'],
+  rules: {
+    // \u81EA\u5B9A\u4E49\u89C4\u5219
+    'no-console': 'warn',
+    'no-unused-vars': 'error',
+  },
+};
+`,paraId:15,tocIndex:10},{value:"\u6700\u4F73\u5B9E\u8DF5",paraId:16,tocIndex:12},{value:": \u5305\u542B\u4EE3\u7801\u6700\u4F73\u5B9E\u8DF5\u76F8\u5173\u89C4\u5219",paraId:16,tocIndex:12},{value:"\u53EF\u80FD\u7684\u9519\u8BEF",paraId:16,tocIndex:12},{value:": \u5E2E\u52A9\u53D1\u73B0\u4EE3\u7801\u4E2D\u6F5C\u5728\u7684\u9519\u8BEF",paraId:16,tocIndex:12},{value:"\u4EE3\u7801\u98CE\u683C",paraId:16,tocIndex:12},{value:": \u7EDF\u4E00\u7684\u4EE3\u7801\u98CE\u683C\u89C4\u8303",paraId:16,tocIndex:12},{value:"\u53D8\u91CF\u76F8\u5173",paraId:16,tocIndex:12},{value:": \u53D8\u91CF\u58F0\u660E\u548C\u4F7F\u7528\u7684\u89C4\u5219",paraId:16,tocIndex:12},{value:"ES6+",paraId:16,tocIndex:12},{value:": ES6 \u53CA\u66F4\u9AD8\u7248\u672C\u7279\u6027\u7684\u4F7F\u7528\u89C4\u8303",paraId:16,tocIndex:12},{value:"\u4E25\u683C\u6A21\u5F0F",paraId:16,tocIndex:12},{value:": \u4E0E\u4E25\u683C\u6A21\u5F0F\u76F8\u5173\u7684\u89C4\u5219",paraId:16,tocIndex:12},{value:"\u5982\u679C\u9047\u5230 ESLint \u89C4\u5219\u4E0E Prettier \u683C\u5F0F\u5316\u7ED3\u679C\u51B2\u7A81\uFF0C\u8BF7\u786E\u4FDD\uFF1A",paraId:17,tocIndex:14},{value:"\u5DF2\u6B63\u786E\u5B89\u88C5 ",paraId:18,tocIndex:14},{value:"eslint-config-prettier",paraId:18,tocIndex:14},{value:" \u548C ",paraId:18,tocIndex:14},{value:"eslint-plugin-prettier",paraId:18,tocIndex:14},{value:"\u5728 ",paraId:18,tocIndex:14},{value:".eslintrc.js",paraId:18,tocIndex:14},{value:" \u7684 ",paraId:18,tocIndex:14},{value:"extends",paraId:18,tocIndex:14},{value:" \u6570\u7EC4\u4E2D\uFF0C",paraId:18,tocIndex:14},{value:"prettier",paraId:18,tocIndex:14},{value:" \u914D\u7F6E\u4F4D\u4E8E\u6700\u540E",paraId:18,tocIndex:14},{value:".prettierrc.js",paraId:18,tocIndex:14},{value:" \u914D\u7F6E\u4E0E\u56E2\u961F\u89C4\u8303\u4E00\u81F4",paraId:18,tocIndex:14},{value:"\u5BF9\u4E8E TypeScript \u9879\u76EE\uFF0C\u786E\u4FDD\uFF1A",paraId:19,tocIndex:15},{value:"\u9879\u76EE\u6839\u76EE\u5F55\u5305\u542B\u6709\u6548\u7684 ",paraId:20,tocIndex:15},{value:"tsconfig.json",paraId:20,tocIndex:15},{value:"\u4F7F\u7528\u6B63\u786E\u7684\u914D\u7F6E\u6269\u5C55\uFF08",paraId:20,tocIndex:15},{value:"typescript",paraId:20,tocIndex:15},{value:"\uFF09",paraId:20,tocIndex:15},{value:"\u5B89\u88C5\u4E86\u6240\u9700\u7684\u4F9D\u8D56\uFF08",paraId:20,tocIndex:15},{value:"@typescript-eslint/parser",paraId:20,tocIndex:15},{value:" \u548C ",paraId:20,tocIndex:15},{value:"@typescript-eslint/eslint-plugin",paraId:20,tocIndex:15},{value:"\uFF09",paraId:20,tocIndex:15}]},30947:function(t,e,n){n.r(e),n.d(e,{texts:function(){return a}});var d=n(7120);const a=[{value:"\u652F\u6301\u914D\u5957\u7684 ",paraId:0,tocIndex:0},{value:"commitlint \u914D\u7F6E",paraId:0,tocIndex:0},{value:"\uFF0C\u7528\u4E8E\u5BF9 ",paraId:0,tocIndex:0},{value:"git commit message",paraId:0,tocIndex:0},{value:" \u8FDB\u884C\u6821\u9A8C\u3002",paraId:0,tocIndex:0},{value:"\u4F7F\u7528\u65F6\uFF0C\u9700\u8981\u5B89\u88C5 ",paraId:1,tocIndex:1},{value:"@commitlint/cli",paraId:1,tocIndex:1},{value:"\uFF1A",paraId:1,tocIndex:1},{value:`npm install @efdev/config-commitlint -D
 `,paraId:2,tocIndex:1},{value:`npm install @commitlint/cli -D
 `,paraId:3,tocIndex:1},{value:"\u5728 ",paraId:4,tocIndex:2},{value:"commitlint.config.js",paraId:4,tocIndex:2},{value:" \u4E2D\u96C6\u6210\u672C\u5305:",paraId:4,tocIndex:2},{value:`module.exports = {
   extends: ['@efdev/config-commitlint'],
